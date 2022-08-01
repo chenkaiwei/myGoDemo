9ecf632a419907bb85c0022748285b054ed21d92
chenkaiwei <chenkaiwei.1988@gmail.com>
2022-08-01 17:59:42 +0800

    v3

--- docker-network/server/server.go
+++ docker-network/server/server.go
@@ -11,7 +11,7 @@ func main() {
 	r := gin.Default()
 	//2.绑定路由规则，执行的函数
 	r.GET("/", func(context *gin.Context) {
-		context.String(http.StatusOK, "您已成功访问到TestServer的1122 端口")
+		context.String(http.StatusOK, "您已成功访问到TestServer的1122 端口 -v3 ")
 	})
 	//3.监听端口，默认8080
 	r.Run(":1122")
